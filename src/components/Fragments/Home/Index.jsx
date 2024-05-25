42b8f2d8bff1996536c0b372ddce119bd6288428
Akhfee Lauki Mahfuda <95747333+UkiMahfuda@users.noreply.github.com>
2024-05-25 18:00:46 +0700

    Update Index.jsx

--- src/components/Fragments/Home/Index.jsx
+++ src/components/Fragments/Home/Index.jsx
@@ -23,11 +23,11 @@ export const Home = () => {
     <Fragment>
       <div className="h-dvh" id="element">
         <div className="flex flex-col justify-center items-center h-dvh bg-primary bg-opacity-75">
-          <div className="text-white text-left sm:text-center container px-5 mx-auto">
+          <div className="text-white text-left sm:text-center container px-5 lg:px-10 mx-auto">
             <p className="montserrat-bold text-sm sm:text-base text-tesier" data-aos="zoom-in">
               Hello, I'm
             </p>
-            <p className="montserrat-alternates-black mb-3 text-3xl sm:text-4xl xl:text-5xl lg:my-4" data-aos="zoom-in" data-aos-delay="50">
+            <p className="montserrat-alternates-black mb-3 text-3xl sm:text-4xl xl:text-5xl lg:my-2 xl:my-4" data-aos="zoom-in" data-aos-delay="50">
               Akhfee Lauki Mahfuda
             </p>
             <p className="montserrat mb-6 text-sm xl:text-base text-gray" data-aos="zoom-in" data-aos-delay="100">
